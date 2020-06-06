934c8444a02ec413c4d11c8e414eebc8db3b211e
s0y00n <leeesoyoon@gmail.com>
2020-06-06 21:37:38 +0900

    upload all

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'page.apps.pageconfig',
+    'page.apps.PageConfig',
 ]
 
 MIDDLEWARE = [
